323f706d956b4cbac5d485defca3c70df9ba57a1
Maochuan <thinkermao@outlook.com>
2016-10-08 15:12:00 +0800

    leancloud SDK init

--- src/main/java/net/hashcoding/scucrawler/Main.java
+++ src/main/java/net/hashcoding/scucrawler/Main.java
@@ -8,6 +8,9 @@ public class Main {
 	public static void main(String[] args) {
         dumpFilename = "D:\\passage_dump.txt";
 
+        // leancloud sdk init.
+        Config.initialize();
+
 		TaskManager manager = TaskManager.instance();
 		manager.delegateTask(new JWCTask());
 		manager.run();
